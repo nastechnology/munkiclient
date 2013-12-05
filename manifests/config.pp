691181bc641a7421ab04cc78a37033a9d3d89b53
Mark Myers <marcusmyers@gmail.com>
2013-12-05 11:30:03 -0500

    Fixed config to NOT use property_list module

--- manifests/config.pp
+++ manifests/config.pp
@@ -5,56 +5,36 @@ class munkiclient::config {
     ensure  => present,
     owner   => root,
     group   => admin,
-    require => Package['munkitools'],
+    require => Package['munkitools-0_9_2_1851_0.dmg'],
   }
 
-  property_list_key { 'SoftwareRepoURL':
-    ensure  => present,
-    path    => '/Library/Preferences/ManagedInstalls.plist',
-    key     => 'SoftwareRepoURL',
-    value   => $software_repo_url,
-    require => File['/Library/Preferences/ManagedInstalls.plist'],
-  }
-  property_list_key { 'SoftwareRepoCACertificate':
-    ensure  => present,
-    path    => '/Library/Preferences/ManagedInstalls.plist',
-    key     => 'SoftwareRepoCACertificate',
-    value   => '/usr/local/munki/munkiwa.nas.local.cert',
-  }
-
-  property_list_key { 'LoggingLevel':
-    ensure => present,
-    path   => '/Library/Preferences/ManagedInstalls.plist',
-    key    => 'LoggingLevel',
-    value  => 1,
-    value_type=> 'integer',
-    require => File['/Library/Preferences/ManagedInstalls.plist'],
-  }
-
-  property_list_key { 'DaysBetweenNotifications':
-    ensure => present,
-    path   => '/Library/Preferences/ManagedInstalls.plist',
-    key    => 'DaysBetweenNotifications',
-    value  => 1,
-    value_type => 'integer',
+  exec { 'SoftwareRepoURL':
+    command => "/usr/bin/defaults write /Library/Preferences/ManagedInstalls SoftwareRepoURL '${software_repo_url}'",
     require => File['/Library/Preferences/ManagedInstalls.plist'],
   }
   
-  property_list_key { 'ClientIdentifier':
-    ensure => present,
-    path   => '/Library/Preferences/ManagedInstalls.plist',
-    key    => 'ClientIdentifier',
-    value  => "${hostname}.nas.local",
-    value_type => 'string',
+  exec { 'SoftwareRepoCACertificate':
+    command => "/usr/bin/defaults write /Library/Preferences/ManagedInstalls SoftwareRepoCACertificate '/usr/local/munki/munkiwa.nas.local.cert'",
     require => File['/Library/Preferences/ManagedInstalls.plist'],
   }
 
-  property_list_key { 'InstallAppleSoftwareUpdates':
-    ensure => present,
-    path   => '/Library/Preferences/ManagedInstalls.plist',
-    key    => 'InstallAppleSoftwareUpdates',
-    value  => true,
-    value_type => 'boolean',
+  exec { 'LoggingLevel':
+    command => "/usr/bin/defaults write /Library/Preferences/ManagedInstalls LoggingLevel -int 1",
+    require => File['/Library/Preferences/ManagedInstalls.plist'],
+  }
+
+  exec { 'DaysBetweenNotifications':
+    command => "/usr/bin/defaults write /Library/Preferences/ManagedInstalls DaysBetweenNotifications -int 1",
+    require => File['/Library/Preferences/ManagedInstalls.plist'],
+  }
+  
+  exec { 'ClientIdentifier':
+    command => "/usr/bin/defaults write /Library/Preferences/ManagedInstalls ClientIdentifier '${hostname}.nas.local'",
+    require => File['/Library/Preferences/ManagedInstalls.plist'],
+  }
+
+  exec { 'InstallAppleSoftwareUpdates':
+    command => "/usr/bin/defaults write /Library/Preferences/ManagedInstalls InstallAppleSoftwareUpdates -bool TRUE',
     require => File['/Library/Preferences/ManagedInstalls.plist'],
   }
 }
